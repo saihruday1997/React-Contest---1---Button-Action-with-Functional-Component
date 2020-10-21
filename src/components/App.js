082deb3c68b23e56fdea4a96dfa7b86e308843a1
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-10-21 21:01:24 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [text, setText] = React.useState("");
+
+  const clickFn = (text) => {
+    setText(text);
+  };
+
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+    <>
+      <div id="main">// Do not alter the main div</div>
+      <button id="click" onClick={clickFn}>
+        CLICK
+      </button>
+    </>
   );
 }
-
 
 export default App;
---
+++ src/components/text.jsx
@@ -0,0 +1,13 @@
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
+
+function text() {
+  return (
+    <p>
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so
+      happy
+    </p>
+  );
+}
+
+export default text;
