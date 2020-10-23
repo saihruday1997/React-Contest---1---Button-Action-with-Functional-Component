bba64e5eef8fc9958aa429a00d6964fca00c9334
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-10-23 13:40:37 +0530

    changes123

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
-import Text from "./Text";
 
 function App() {
-  const [text, setText] = React.useState("");
+  const [display, setDisplay] = React.useState(false);
 
   const textData =
     "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
 
-  const clickFn = (textData) => {
-    setText(textData);
+  const clickFn = () => {
+    setDisplay(true);
   };
 
   return (
     <>
       <div id="main">// Do not alter the main div</div>
-      <button id="click" onClick={() => clickFn(textData)}>
+      <button id="click" onClick={clickFn}>
         CLICK
       </button>
-      <Text text={text} />
+      {display && <p id="para">{textData}</p>}
     </>
   );
 }
--- src/components/Text.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component, useState } from "react";
-import "./../styles/App.css";
-
-function Text(text) {
-  return <p>{text}</p>;
-}
-
-export default Text;
